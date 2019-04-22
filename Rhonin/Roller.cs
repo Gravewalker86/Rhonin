49976fbac9edad0262b0514e832e78e581214258
GraveWalker <GraveWalker8086@gmail.com>
2019-04-22 13:55:08 -0400

    Seperated Roller(s) into their own namespace. Rhonin.RNG.
    Also added MACHINE_EPSILION constant which is needed for several RNG approaches.

---
+++ Rhonin/Roller.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rhonin.RNG
+{
+
+    class DiceRoller
+    {
+    }
+
+    public class Dice
+    {
+        private const double MACHINE_EPSILION = 0.000000000000001d; //needed for floor function to work in 0 < rand < 1 based RNG.
+        private int _sides = -1;
+        private List<int> _rollTable = new List<int>();
+        private SortedDictionary<Guid, int> _sortTable = new SortedDictionary<Guid, int>();
+
+        public Dice(int inputSides)
+        {
+            _sides = inputSides;
+            _generateTable();
+        }
+
+        private void _generateTable()
+        {
+            for (int i = 1; i <= _sides; i++)
+            {
+
+            }
+        }
+
+
+    }
+}
