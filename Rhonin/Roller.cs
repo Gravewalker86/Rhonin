ee8f1ce810e6545625652a6ffc782980c9aa7b7b
GraveWalker <GraveWalker8086@gmail.com>
2019-04-23 08:31:49 -0400

    Finished table generation, code is a mess from testing.

--- Rhonin/Roller.cs
+++ Rhonin/Roller.cs
@@ -12,6 +12,9 @@ namespace Rhonin.RNG
     {
         public void Start()
         {
+            var timer = new System.Diagnostics.Stopwatch();
+            timer.Start();
+
             LookupDie D4 = new LookupDie(4);
             LookupDie D6 = new LookupDie(6);
             LookupDie D8 = new LookupDie(8);
@@ -19,8 +22,12 @@ namespace Rhonin.RNG
             LookupDie D12 = new LookupDie(12);
             LookupDie D20 = new LookupDie(20);
             LookupDie D100 = new LookupDie(100);
+            timer.Stop();
 
-            D100._dumpTable();
+            D20._dumpTable();
+            Console.WriteLine($"Tables generated in: {timer.Elapsed.TotalMilliseconds} Milliseconds.");
+
+            
         }
     }
 
@@ -39,7 +46,7 @@ namespace Rhonin.RNG
             _generateTable();
         }
 
-        private void _generateTable()
+        private void _generateTable()//generates the shuffled lookup table for the die.
         {
             for (int x = 1; x <= _sides; x++)//loop for each side of the die.
             {
@@ -50,13 +57,20 @@ namespace Rhonin.RNG
                     _sortTable.Add(new Guid(_guid), x);
                 }
             }
+
+            foreach(var entry in _sortTable) //converting sorted dictionary to list for faster lookup.
+            {
+                _rollTable.Add(entry.Value);
+            }
+
+            _sortTable.Clear(); //clearing sorted dictionary for GC.
         }
 
         public void _dumpTable()
         {
-            foreach(var entry in _sortTable)
+            foreach(var entry in _rollTable)
             {
-                Console.WriteLine($"Key: {entry.Key.ToString()}, Value: {entry.Value}");
+                Console.WriteLine($"Value: {entry}");
             }
         }
     }
